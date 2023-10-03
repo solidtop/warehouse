ff2a744cd7cba1b5ffee63c453eb518b5520c84f
Axel Asp <solidtop.asp@gmail.com>
2023-10-03 13:38:04 +0200

    Add basic product api endpoints

--- src/main/java/com/example/warehouse/resources/ProductResource.java
+++ src/main/java/com/example/warehouse/resources/ProductResource.java
@@ -1,24 +1,32 @@
 package com.example.warehouse.resources;
 
 import com.example.warehouse.entities.Product;
+import com.example.warehouse.entities.ProductCategory;
 import com.example.warehouse.service.ProductService;
 import com.example.warehouse.service.Warehouse;
+import com.example.warehouse.validators.ProductValidator;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
+import jakarta.validation.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Request;
+import jakarta.ws.rs.core.Response;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Path("/products")
 @Produces(MediaType.APPLICATION_JSON)
 public class ProductResource {
-    ProductService productService;
+    private ProductService productService;
 
-    public ProductResource(){}
+    public ProductResource() {
+    }
 
     @Inject
     public ProductResource(ProductService productService) {
@@ -26,13 +34,50 @@ public class ProductResource {
     }
 
     @GET
-    public List<Product> getAllProducts() {
-        return productService.getAllProducts();
+    public Response getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return Response.ok(products).build();
     }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addNewProduct(@Valid ProductRequest productRequest) { //TODO: Fix category conversion
+        String name = productRequest.name();
+        ProductCategory category = ProductCategory.valueOf(productRequest.category().toUpperCase());
+        int rating = productRequest.rating();
+        Product product = productService.addNewProduct(name, category, rating);
+        return Response.ok(product).build();
+    }
+
+    //@PUT
 
     @GET
     @Path("/{id}")
-    public Optional<Product> getProductById(@PathParam("id") String id) {
-        return productService.getProductById(id);
+    public Response getProductById(@PathParam("id") String id) {
+        Optional<Product> productOptional = productService.getProductById(id);
+        if (productOptional.isEmpty()) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        return Response.ok(productOptional.get()).build();
+    }
+
+    @GET
+    @Path("/categories")
+    public Response getAllCategories() {
+        Set<ProductCategory> categories = productService.getCategoriesWithProducts();
+        return Response.ok(categories).build();
+    }
+
+    @GET
+    @Path("/categories/{category}")
+    public Response getProductsByCategory(@PathParam("category") String category) {
+        try {
+            ProductCategory productCategory = ProductCategory.valueOf(category.toUpperCase());
+            List<Product> products = productService.getProductsByCategory(productCategory);
+            return Response.ok(products).build();
+        } catch (IllegalArgumentException e) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
     }
 }
